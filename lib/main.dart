1e069654d960ae831be38dcc483dcbb34da477b4
Irakli <iraklibarbaqadze241995@gmail.com>
2021-01-06 23:02:37 +0400

    create Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -20,20 +20,45 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.yellowAccent,
         ),
-      body:Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('hello world'),
-          FlatButton(onPressed: () {}, 
-            child: Text('Click me'),
-            color: Colors.lightBlue,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Container(
+                child: Text('everyone'),
+                padding: EdgeInsets.all(30.0),
+                color: Colors.lightGreenAccent,
+              ),
+              Text('hello',
+              style: TextStyle(
+                backgroundColor: Colors.indigo
+              ),),
+              Text('world',
+                style: TextStyle(
+                    backgroundColor: Colors.green
+                ),)
+            ],
           ),
           Container(
-            child: Text('some text'),
+            child: Text('one'),
+            color: Colors.lightBlue,
+            padding: EdgeInsets.all(20.0),
+          ),
+          Container(
+            child: Text('two'),
+            color: Colors.amber,
+            padding: EdgeInsets.all(35.0),
+          ),
+          Container(
+            child: Text('three'),
             color: Colors.deepOrange,
-            padding: EdgeInsets.all(40.0),
-          )
+            padding: EdgeInsets.all(50.0),
+          ),
         ],
       ),
 
