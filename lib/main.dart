5dd5f2f16a14b69df430342b86fdff681d170f80
Irakli <iraklibarbaqadze241995@gmail.com>
2021-01-06 16:10:52 +0400

    using statless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title:Text(
-            'this is my first App',
-          style: TextStyle(
-            fontSize: 14,
-            color: Colors.black
-          ),
-        ),
-            centerTitle: true,
-        backgroundColor: Colors.yellowAccent,
-      ),body:Center(
-      child:Text(
-          'Irakklis App',
-        style:TextStyle(
-          fontSize: 19.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 3.0,
-          fontFamily: 'IndieFlower'
-        )
-      ),
-    ),floatingActionButton: FloatingActionButton(
-      child:Text('add'),
-      backgroundColor: Colors.yellowAccent,
-    ),
-      backgroundColor: Colors.lightBlue[700]
-    )
+    home:Home(),
   ));
 }
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title:Text(
+            'this is my first App',
+            style: TextStyle(
+                fontSize: 14,
+                color: Colors.black
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.yellowAccent,
+        ),body:Center(
+      child:Text(
+          'Irakklis App',
+          style:TextStyle(
+              fontSize: 19.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3.0,
+              fontFamily: 'IndieFlower'
+          )
+      ),
+    ),floatingActionButton: FloatingActionButton(
+      child:Text('click'),
+      backgroundColor: Colors.yellowAccent,
+    ),
+        backgroundColor: Colors.lightBlue[700]
+    );
+  }
+}
+
+
